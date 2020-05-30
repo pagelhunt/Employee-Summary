1b654a9a96b6c82f3376531be6356734f9a214d8
page hunt <pagelhunt@gmail.com>
2020-05-30 06:36:50 -0600

    Added classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+​class Employee
+{
+    constructor(name,id,email)
+    {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+    
+    getName() 
+    {
+        return this.name;
+    }
+
+    getId()
+    {
+        return this.id;
+    }
+
+    getEmail()
+    {
+        return this.email;
+    }
+
+    getRole()
+    {
+        return employee;
+    }
+}
+module.exports = Employee;
